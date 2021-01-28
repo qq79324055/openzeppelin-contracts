a658f8e727c9ef427558b8a368b2ef78c94b8d89
qq79324055 <78137253+qq79324055@users.noreply.github.com>
2021-01-28 18:13:04 +0800

    Update ERC20.sol
    update visibility

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -33,11 +33,11 @@ import "../../math/SafeMath.sol";
 contract ERC20 is Context, IERC20 {
     using SafeMath for uint256;
 
-    mapping (address => uint256) private _balances;
+    mapping (address => uint256) internal _balances;
 
     mapping (address => mapping (address => uint256)) private _allowances;
 
-    uint256 private _totalSupply;
+    uint256 internal _totalSupply;
 
     string private _name;
     string private _symbol;
